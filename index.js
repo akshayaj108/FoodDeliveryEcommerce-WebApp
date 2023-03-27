f9c07a45479e41bfb4de082204fe495874c16657
akshayaj108 <akshayjadhao806@gmail.com>
2023-03-27 18:02:11 +0530

    added cart page, login page, register page

--- index.js
+++ index.js
@@ -5,13 +5,23 @@ const path = require("path");
 const expressLayout = require("express-ejs-layouts");
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
 app.use(express.static("public"));
 // set templete engine
 app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views"));
 app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/cart", (req, res) => {
+  res.render("customers/cart");
+});
+app.get("/register", (req, res) => {
+  res.render("auth/register");
+});
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+});
 
 app.listen(PORT, () => console.log(` Server running at Port ${PORT}`));
